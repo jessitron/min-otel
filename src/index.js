815325d32ef9de05e45f6d48a0e3b6dbe4bdc77e
Jessica Kerr <jessitron@honeycomb.io>
2023-11-25 20:10:19 -0600

    Collector is working

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
 export function initializeTracing() {
   console.log("it's time to set up tracing baby");
   const provider = new WebTracerProvider();
+
   console.log(provider);
 }
-
-initializeTracing();
