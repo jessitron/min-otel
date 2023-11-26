5b139c9b3927367533e62bfeea50c7455bc3ce8f
Jessica Kerr <jessitron@honeycomb.io>
2023-11-25 19:50:35 -0600

    Be sure to use it somehow

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { WebTracingProvider } from "@opentelemetry/sdk-trace-web";
 export function initializeTracing() {
   console.log("it's time to set up tracing baby");
   const provider = new WebTracerProvider();
+  console.log(provider);
 }
 
 initializeTracing();
