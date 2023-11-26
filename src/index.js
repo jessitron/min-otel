342c5c0da6a95df81f93c84948194bbd93db0ed3
Jessica Kerr <jessitron@honeycomb.io>
2023-11-25 19:44:42 -0600

    I can call the module this way but it doesn't make it bundle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { WebTracingProvider } from "@opentelemetry/sdk-trace-web";
 
-function initializeTracing() {
+export function initializeTracing() {
   console.log("it's time to set up tracing baby");
   const provider = new WebTracerProvider();
 }
-
-window.initializeTracing = initializeTracing;
