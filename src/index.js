6fa5b4503cd94cd87db7acddcca54750cae1f049
Jessica Kerr <jessitron@honeycomb.io>
2023-11-25 19:22:31 -0600

    make it have a single, specific target

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-export function initializeTracing() {
+import { WebTracingProvider } from "@opentelemetry/sdk-trace-web";
+
+function initializeTracing() {
   console.log("it's time to set up tracing baby");
 }
+
+window.initializeTracing = initializeTracing;
