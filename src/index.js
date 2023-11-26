720bc3ec807b6e9a4816b5b4305b9643f6541b58
Jessica Kerr <jessitron@honeycomb.io>
2023-11-25 19:27:15 -0600

    It doesn't work. It doesn't bundle anything

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { WebTracingProvider } from "@opentelemetry/sdk-trace-web";
 
 function initializeTracing() {
   console.log("it's time to set up tracing baby");
+  const provider = new WebTracerProvider();
 }
 
 window.initializeTracing = initializeTracing;
