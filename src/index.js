6103d581c214ddf470f63a22ee280a8bf14f95d5
Jessica Kerr <jessitron@honeycomb.io>
2023-11-25 19:51:50 -0600

    OMG it was a misspelling all along

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { WebTracingProvider } from "@opentelemetry/sdk-trace-web";
+import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
 
 export function initializeTracing() {
   console.log("it's time to set up tracing baby");
